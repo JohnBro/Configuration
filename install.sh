820951e469f63cdd0cb8431d46d078599c831d9c
JohnBro <lujunboo@gmail.com>
2019-04-07 16:51:11 +0800

    Add for sync.

--- install.sh
+++ install.sh
@@ -31,20 +31,20 @@ fi
 ################################################################
 TBL_NAT=(
 $TOP/$VIM_NAT
-$TOP/$GIT_NAT
+#$TOP/$GIT_NAT
 $TOP/$TMU_NAT
-$TOP/$BAS_NAT
-$TOP/$SPACEMACS_NAT
+#$TOP/$BAS_NAT
+#$TOP/$SPACEMACS_NAT
 $TOP/$ZSH_NAT
 )
 
 ###################### Config file table ########################
 TBL_COF=(
 $HOME/$VIM_COF
-$HOME/$GIT_COF
+#$HOME/$GIT_COF
 $HOME/$TMU_COF
-$HOME/$BAS_COF
-$HOME/$SPACEMACS_COF
+#$HOME/$BAS_COF
+#$HOME/$SPACEMACS_COF
 $HOME/$ZSH_COF
 )
 
